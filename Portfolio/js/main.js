0ea074a1a04134e13e9421c24c7ff7868323fddf
AmpRack <rob.hill@gmail.com>
2015-02-28 00:16:50 -0800

    Project Section Buttons
    All projects are properly animated. Carousel replaced; no further
    issues. Some js bug fixes and clean up.

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -23,7 +23,7 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Resize groups of divs to an equal height        
+// Resize the #about btn-content divs to an equal height        
 equalheight = function(container){
 
 var currentTallest = 0,
@@ -31,7 +31,7 @@ var currentTallest = 0,
      rowDivs = new Array(),
      $el,
      topPosition = 0;
- $('.btn-content').each(function() {
+ $('#about .btn-content').each(function() {
 
    $el = $(this);
    $($el).height('auto')
@@ -55,11 +55,100 @@ var currentTallest = 0,
  });
 }
 
+
+
+
+
 $(window).load(function() {
   equalheight('.main article');
 });
 
-
 $(window).resize(function(){
   equalheight('.main article');
+  $('#carousel li img').css({ width: $(window).width() });
+  $('#carousel').css({ marginLeft: - $(window).width() });
 });
+
+function runCarousel() {
+  var slideWidth = $('#carousel li').width();
+
+  $('#carousel').animate({
+      left: - slideWidth
+  }, 1000, function () {
+      $('#carousel li:first-child').appendTo('#carousel');
+      $('#carousel').css('left', '');
+  });
+}
+// Initiate Carousel
+$(document).ready(function() {
+  var slideCount = $('#carousel li').length;
+  var slideWidth = $(window).width();
+  var slideHeight = $('#carousel li').height();
+  var sliderUlWidth = slideCount * slideWidth;
+
+  $('#img-carousel').css({ width: slideWidth });
+  $('#carousel').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+  $('#carousel li img').css({ width: $(window).width() });
+    setInterval(function () {
+      runCarousel();
+  }, 5000);
+})
+
+
+// Animate Digital Clock button on hover
+function clockBlink() {
+  var currentTime = new Date();
+  var secs = currentTime.getSeconds();
+
+  if (secs % 2 == 1) {
+      $('#project-clock p').css('border-width', "4px");
+      $('#project-clock p').css('padding', "3px 6px");
+  } else {
+      $('#project-clock p').css('border-width', "2px");
+      $('#project-clock p').css('padding', "5px 9px");
+  }
+  setTimeout(function() {clockBlink()}, 1000);
+}
+$(window).ready(function() {
+  // Detect hover over Digital Clock
+  $('.project-item:nth-of-type(1)').mouseover(function(){
+    $('#project-clock p').css('border-color', "#05E9FF");
+    clockBlink();
+  })
+  $('.project-item:nth-of-type(1)').mouseout(function(){
+    $('#project-clock p').css('border-color', "#000000");
+  })
+
+  // Detect hover over Calculator
+  $('.project-item:nth-of-type(2)').mouseover(function(){
+    $('#calc-bg').css('bottom', '90.25%');
+    $('#calc-keys').css('margin-top','44%')
+  })
+  $('.project-item:nth-of-type(2)').mouseout(function(){
+    $('#calc-bg').css('bottom', '-40%');
+    $('#calc-keys').css('margin-top','30%');
+  })
+
+  // Detect hover over Vault Shop
+  $('.project-item:nth-of-type(3)').mouseover(function(){
+    $('#project-vault p:first-of-type').css('top', '-80px');
+    $('#project-vault p:last-of-type').css('top','-70px')
+  })
+  $('.project-item:nth-of-type(3)').mouseout(function(){
+    $('#project-vault p:first-of-type').css('top', '-210px');
+    $('#project-vault p:last-of-type').css('top','40px')
+  })
+
+  // Detect hover over Simulator
+  var switchCells = [0,1,4,5,7,15];
+  $('.project-item:nth-of-type(4)').mouseover(function(){
+    $.each(switchCells, function(i, x){
+      $('.sim-cell:nth-of-type(' + x + ')').toggleClass('cell-black');
+    })
+  })
+  $('.project-item:nth-of-type(4)').mouseout(function(){
+    $.each(switchCells, function(i, x){
+      $('.sim-cell:nth-of-type(' + x + ')').toggleClass('cell-black');
+    })
+  })
+})
